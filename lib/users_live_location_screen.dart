8ff9a69be73d25c90f24743ab96df486a3e2e145
Ammar Mohamed <ammar0xf@gmail.com>
2025-03-06 13:59:47 +0200

    code magic

--- lib/users_live_location_screen.dart
+++ lib/users_live_location_screen.dart
@@ -10,28 +10,26 @@ class UsersLiveLocationScreen extends StatefulWidget {
 
 class _UsersLiveLocationScreenState extends State<UsersLiveLocationScreen> {
   final DatabaseReference _usersRef = FirebaseDatabase.instance.ref("users");
-  Map<String, Marker> _markers = {};
+  List<Marker> _markers = [];
 
   @override
   void initState() {
     super.initState();
     _usersRef.onValue.listen((event) {
       final users = event.snapshot.value as Map<dynamic, dynamic>;
-      final markers = <String, Marker>{};
+      final markers = <Marker>[];
       users.forEach((key, value) {
         final location = value["current_location"];
         if (location["latitude"] != "" && location["longitude"] != "") {
           final lat = double.parse(location["latitude"]);
           final lng = double.parse(location["longitude"]);
           final marker = Marker(
+            point: LatLng(lat, lng),
             width: 80.0,
             height: 80.0,
-            point: LatLng(lat, lng),
-            builder: (ctx) => Container(
-              child: Icon(Icons.location_on, color: Colors.red, size: 40),
-            ),
+            builder: (ctx) => Icon(Icons.location_on, color: Colors.red, size: 40),
           );
-          markers[key] = marker;
+          markers.add(marker);
         }
       });
       setState(() {
@@ -55,7 +53,7 @@ class _UsersLiveLocationScreenState extends State<UsersLiveLocationScreen> {
             subdomains: ['a', 'b', 'c'],
           ),
           MarkerLayer(
-            markers: _markers.values.toList(),
+            markers: _markers,
           ),
         ],
       ),
